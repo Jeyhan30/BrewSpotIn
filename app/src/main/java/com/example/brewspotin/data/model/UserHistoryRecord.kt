2a6a7ae6c4f15ba4c0eb2f8612cd86eec4ab723b
Jeyhan Farrel Alfarisqi <167233760+Jeyhan30@users.noreply.github.com>
2025-06-25 07:47:41 +0700

    Update UserHistoryRecord.kt

--- app/src/main/java/com/example/brewspotin/data/model/UserHistoryRecord.kt
+++ app/src/main/java/com/example/brewspotin/data/model/UserHistoryRecord.kt
@@ -5,26 +5,25 @@ import com.google.firebase.firestore.DocumentId
 import com.google.firebase.firestore.ServerTimestamp
 import java.util.Date
 
-// Data class untuk setiap item di dalam array 'items' pada UserHistoryRecord
 data class OrderItem(
     val cafeId: String = "",
     val description: String = "",
     val imageUrl: String = "",
     val menuItemId: String = "",
     val name: String = "",
-    val priceAtOrder: Long = 0, // Harga per item saat dipesan (numeric)
-    val quantity: Long = 0 // Kuantitas (numeric)
+    val priceAtOrder: Long = 0,
+    val quantity: Long = 0
 )
 
 data class UserHistoryRecord(
     @DocumentId
-    val id: String = "", // ID dokumen dari koleksi 'history' (User View)
+    val id: String = "",
     val appFeeAmount: Long = 0,
     val cafeId: String = "",
     val cafeName: String = "",
     val downPaymentAmount: Long = 0,
     val items: List<OrderItem> = emptyList(), // List of OrderItem
-    val reservationId: String = "", // Ini adalah ID dokumen dari koleksi 'reservations' yang terkait
+    val reservationId: String = "",
     @ServerTimestamp
     val timestamp: Date? = null,
     val totalMenuOrderPrice: Long = 0, // Total harga pesanan menu
@@ -32,5 +31,5 @@ data class UserHistoryRecord(
     val userEmail: String = "",
     val userId: String = "",
     val userPhone: String = "",
-    val username: String = "" // Nama pengguna dari history (bukan dari reservations)
+    val username: String = ""
 )
